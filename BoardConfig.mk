f974cf57278a8972e69456cdfbdb25e79b82b7a8
Rúben Carneiro <rubencarneiro01@gmail.com>
2020-12-17 12:42:13 +0000

    Define custom charging res

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,9 @@ TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Custom Charge images
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := device/lge/hammerhead/charger/images
+
 TARGET_RECOVERY_FSTAB = device/lge/hammerhead/fstab.hammerhead
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/hammerhead
